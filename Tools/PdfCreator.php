1e7e9ec9d5ecd17c9bc51eaac9374f534f262068
boutmansb <benjamin@tactics.be>
2014-07-03 17:03:33 +0200

    Basis lay-out factuur klaar, nu nog uitlezen invoice.

--- Tools/PdfCreator.php
+++ Tools/PdfCreator.php
@@ -3,6 +3,10 @@
 namespace Tactics\InvoiceBundle\Tools;
 
 use ZendPdf\PdfDocument;
+use ZendPdf\Page;
+use ZendPdf\Font;
+use ZendPdf\Color\Html;
+use ZendPdf\Image;
 use Tactics\InvoiceBundle\Model\Invoice;
 
 class PdfCreator
@@ -10,22 +14,123 @@ class PdfCreator
   public static function generatePdf(Invoice $invoice)
   {
     $pdf = new PdfDocument();
-    
+    $pdf->pages[0] = new Page( Page::SIZE_A4 ); // print marge van 10/13 mm Max dimensions A4 = 210mm x 297mm / 595.28 Pts 841.89 Pts
+
+    //standaard gebruikt font.
+    $font = Font::fontWithName(Font::FONT_HELVETICA);
+
+    //logo
+    $logo = Image::imageWithPath('images/logo-provant.png');
+
+    ///////////
+    ////////// Kleuren:
+    /////////
+    $cAlert = Html::color('#FF2A00');
+    $cDraw = Html::color('#424242');
+    $cText = Html::color('#5C5C5C');
+    $cLightText = Html::color('#ADADAD');
+    $cWhite = Html::color('#FFFFFF');
+
+    ///////////
+    ////////// Header:
+    /////////
+    //logo
+    $pdf->pages[0]
+      ->drawImage( $logo, self::mmToPoints(13), 750, 189, 802);
+
+    // Factuur + Factuur datum
+    $pdf->pages[0]
+      ->setFont( $font , 12 )
+      ->setFillColor($cLightText)
+      ->drawText( 'Factuur ##', self::mmToPoints(13), 730 );
+    $pdf->pages[0]
+      ->setFont( $font, 10)
+      ->drawText( date('d/m/Y'), self::mmToPoints(13), 715 );
+
+    // Onze gegevens
+    $pdf->pages[0]
+      ->setFillColor($cText)
+      ->setFont( $font , 14 )
+      ->drawText( 'Inovant', 300, 780 );
+    $pdf->pages[0]
+      ->setFont( $font , 10 )
+      ->drawText( 'Kerkstraat 115', 300, 755 )
+      ->drawText( '2940 Hoevenen', 300, 740 )
+      ->drawText ( 'BTW ############' , 300, 725);
+
+    //Ontvanger
+    $pdf->pages[0]
+      ->setFont( $font , 14 )
+      ->drawText( 'Benjamin Boutmans', 300, 665 );
+    $pdf->pages[0]
+      ->setFont( $font , 10 )
+      ->drawText( 'Lange Sterrestraat 53 Bus 11', 300, 640 )
+      ->drawText( '2180 Ekeren', 300, 625 )
+      ->drawText ( '################' , 300, 610);
+
+    ///////////
+    ////////// Items:
+    /////////
+    //Items Header
+    $pdf->pages[0]
+      ->setLineColor( $cDraw )
+      ->setFillColor( $cDraw )
+      ->drawRectangle( 0, 530, 510, 570 );
+    $pdf->pages[0]
+      ->setFont( $font, 10 )
+      ->setFillColor( $cWhite )
+      ->setLineColor( $cLightText )
+      ->setLineWidth( 0.5 )
+      ->drawText( 'Omschrijving ' , self::mmToPoints(13) , 546 )
+      ->drawLine( 200, 530, 200, 570 )
+      ->drawText( '#', 210, 546 )
+      ->drawLine( 230, 530, 230, 570 )
+      ->drawText( 'Prijs/stuk' , 240, 546 )
+      ->drawLine( 290 , 530, 290, 570 )
+      ->drawText( 'Prijs (Excl)' , 300, 546 )
+      ->drawLine( 360, 530, 360, 570 )
+      ->drawText( 'BTW (%)', 370, 546 )
+      ->drawLine( 420, 530, 420, 570 )
+      ->drawText( 'Prijs (Incl)', 450, 546 );
+
+    //Items
+    $i = 0;
+    $items = $invoice->getItems();
+    foreach($items as $item)
+    {
+      $pdf->pages[0]
+        ->drawLine( 200, 500 - (30 * $i), 200, 530 - (30 * $i) )
+        ->drawLine( 230, 500 - (30 * $i), 230, 530 - (30 * $i) )
+        ->drawLine( 290, 500 - (30 * $i), 290, 530 - (30 * $i) )
+        ->drawLine( 360, 500 - (30 * $i), 360, 530 - (30 * $i) )
+        ->drawLine( 420, 500 - (30 * $i), 420, 530 - (30 * $i) )
+        ->drawLine( 510, 500 - (30 * $i), 510, 530 - (30 * $i) )
+        ->drawLine( 0, 500 - (30 * $i), 510, 500 - (30 * $i) );
+
+      $pdf->pages[0]
+        ->setFillColor( $cText )
+        ->drawText( 'test', self::mmToPoints(13), 510 - (30 * $i) )
+        ->drawText( 'test', 210, 510 - (30 * $i) )
+        ->drawText( 'test', 240, 510 - (30 * $i) )
+        ->drawText( 'test', 300, 510 - (30 * $i) )
+        ->drawText( 'test', 370, 510 - (30 * $i) )
+        ->drawText( 'test', 450, 510 - (30 * $i) );
+      $i++;
+    }
+
+
+//    $pdf->save( 'example.pdf' );
     return $pdf;
   }
-  
-  public static function stream($pdf, $filename)
-  {
-    $response = \sfContext::getInstance()->getResponse();
 
-    $response->setContentType('application/pdf; charset=utf-8');
-    $response->setHttpHeader('Content-Language', 'nl');
-    $response->addVaryHttpHeader('Accept-Language');
-    $response->addCacheControlHttpHeader('no-cache');
-    $response->setHttpHeader('Content-Disposition', 'attachment; filename=' . $filename);
-    
-    echo $pdf->render();    
+  private function pointsToMm( $points )
+  {
+    return $points / 72 * 25.4;
   }
-  
+
+  private function mmToPoints( $mm )
+  {
+    return $mm / 25.4 * 72;
+  }
 }
 
