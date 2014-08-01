4234ea12794c41c78bb1260762bf2bcab49809a4
JorisH <joris@tactics.be>
2014-08-01 14:49:00 +0200

    [ENHANCE] invoicemanager: uitbreiding ifv pro-acc import

--- Propel/InvoiceManager.php
+++ Propel/InvoiceManager.php
@@ -14,7 +14,7 @@ class InvoiceManager extends ObjectManager
      * @param \Tactics\InvoiceBundle\Model\InvoiceableInterface $object
      * @return Tactics\InvoiceBundle\Model\Invoice
      * 
-     * @todo set date_due according to some business logic
+     * @todo setNumber in transactie gieten? dagboek + code moet unique zijn => indien error opnieuw proberen
      */
     public function create(InvoiceableInterface $object = null)
     {
@@ -27,10 +27,14 @@ class InvoiceManager extends ObjectManager
                 $invoice->addItem($item);
             }
             $invoice->setCustomer($object->getCustomer());
+            $invoice->setJournalCode($object->getJournalCode());
         }
         
         $invoice->setDate(time());
         $invoice->setDateDue(strtotime('+30 days'));
+        
+        $invoice->setNumber($this->generateNumber($invoice));
+        $invoice->setStructuredCommunication($this->generateStructuredCommunication($invoice));
                 
         return $invoice;
     }
@@ -40,5 +44,44 @@ class InvoiceManager extends ObjectManager
         $pdfCreator = new PdfCreator();
         
         return $pdfCreator->createPdf($invoice);
+    }
+    
+    private function generateNumber($invoice)
+    {
+        $lastInvoice = $this->searchOne(
+            array(
+                'journal_code' => $invoice->getJournalCode()
+            ),
+            'number', // sort by number
+            false // descending
+        );
+        
+        return $lastInvoice ? $lastInvoice->getNumber() + 1 : 1;
+    }
+    
+    /**
+     * @param \Tactics\InvoiceBundle\Model\Invoice $invoice
+     * @return string structured communication
+     * @throws sfException
+     */
+    private function generateStructuredCommunication(Invoice $invoice)
+    {
+        $strCom = substr($invoice->getJournalCode(), 0, 1);  // first digit = journal code
+        $strCom .= date('y'); // last 2 numbers of full year
+        $strCom .= sprintf("%07u",  $invoice->getNumber()); // zero padded invoice number        
+
+        if (strlen($strCom) != 10) {
+            throw new \sfException('There was a problem generating the structured communication: ' . $strCom);
+        }
+
+        $digit97 = sprintf("%02u", (bcmod($strCom, 97)));
+
+        // Geen 00 als digit97
+        if ($digit97 == 0)
+        {
+            $digit97 = 97;
+        }
+
+        return $strCom . (string) $digit97;
     }
 }
