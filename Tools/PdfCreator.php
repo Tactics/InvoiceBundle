e58c6dcfdc0a0c27ae6634c7f636a7f5afa062de
boutmansb <benjamin@tactics.be>
2014-09-23 15:21:33 +0200

    [TASK] aanpassingen pdf creator voor template.

--- Tools/PdfCreator.php
+++ Tools/PdfCreator.php
@@ -15,16 +15,14 @@ class PdfCreator
   public function createPdf(Invoice $invoice)
   {
     \Misc::use_helper('Helper', 'Number');
-    $pdf = new PdfDocument();
-    $pdf->pages[0] = new Page( Page::SIZE_A4 ); // print marge van 10/13 mm Max dimensions A4 = 210mm x 297mm / 595.28 Pts 841.89 Pts
+    $pdf = PdfDocument::load(SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'ttInvoice' . DIRECTORY_SEPARATOR . 'template.pdf');
+    // print marge van 10/13 mm Max dimensions A4 = 210mm x 297mm / 595.28 Pts 841.89 Pts
 
     $eindtotaal = 0;
 
     //standaard gebruikt font.
     $font = Font::fontWithName(Font::FONT_HELVETICA);
-
-    //logo
-    $logo = Image::imageWithPath('ttInvoice/images/provant-logo.png');
+    $overschrijvingsFont = Font::fontWithName(Font::FONT_COURIER_BOLD);
 
     //Currency, euro standaard
     $currency = $invoice->getCurrency() ? $invoice->getCurrency() : 'EUR';
@@ -34,125 +32,94 @@ class PdfCreator
     /////////
     $cAlert = Html::color('#FF2A00');
     $cDraw = Html::color('#424242');
-    $cText = Html::color('#5C5C5C');
-    $cLightText = Html::color('#ADADAD');
+    $cText = Html::color('#000000');
+    $cLightText = Html::color('#000000');
     $cWhite = Html::color('#FFFFFF');
 
     ///////////
     ////////// Header:
     /////////
-    //logo
-    $pdf->pages[0]
-      ->drawImage( $logo, $this->mmToPoints(13), 750, $this->mmToPoints(13) + 190, 813);
-
-    // Factuur + Factuur datum
-    $pdf->pages[0]
-      ->setFont( $font , 12 )
-      ->setFillColor($cLightText)
-      ->drawText( 'Factuur ' . $invoice->getNumber(), $this->mmToPoints(16), 730 );
-    $pdf->pages[0]
-      ->setFont( $font, 10)
-      ->drawText( $invoice->getDate('d-m-Y'), $this->mmToPoints(16), 715 );
-
     // Onze gegevens
     $pdf->pages[0]
       ->setFillColor($cText)
       ->setFont( $font , 14 )
-      ->drawText( 'APB SPORT', 300, 780 );
+      ->drawText( 'APB SPORT', $this->mmToPoints(13), 805 );
     $pdf->pages[0]
       ->setFont( $font , 10 )
-      ->drawText( 'Boomgaardstraat 22 - 2600 Berchem', 300, 755 )
-      ->drawText( 'Tel. 03 2406270 Fax. 032406299', 300, 740 )
-      ->drawText( 'BTW BE 0820.377.203' , 300, 725)
-      ->drawText( 'Bank 731-0077943-27' , 300, 710)
-      ->drawText( 'IBAN BE48 7310 0779 4327 BIC KREDBEBB' , 300, 695);
+      ->drawText( 'Boomgaardstraat 22 - 2600 Berchem', $this->mmToPoints(13), 790 )
+      ->drawText( 'Tel. 03 2406270 Fax. 032406299', $this->mmToPoints(13), 779 )
+      ->drawText( 'BTW BE 0820.377.203' , $this->mmToPoints(13), 768 )
+      ->drawText( 'Bank 731-0077943-27' , $this->mmToPoints(13), 757 )
+      ->drawText( 'IBAN BE48 7310 0779 4327 BIC KREDBEBB' , $this->mmToPoints(13), 746 );
 
     //Ontvanger
     $costumer = $invoice->getCustomer();
     $pdf->pages[0]
       ->setFont( $font , 14 )
-      ->drawText( $costumer->getNaam(), 300, 665 );
+      ->drawText( $costumer->getNaam(), 350, 730 );
     $pdf->pages[0]
       ->setFont( $font , 10 )
-      ->drawText( $costumer->getStraat() . ' ' . $costumer->getNummer() . ' ' . $costumer->getBus(), 300, 640 )
-      ->drawText( $costumer->getPostcode() . ' ' . $costumer->getGemeente() . ' ' . $costumer->getLandId(), 300, 625 )
-      ->drawText( 'IBAN ' . $costumer->getRekeningnummerIban() , 300, 610);
+      ->drawText( $costumer->getStraat() . ' ' . $costumer->getNummer() . ' ' . $costumer->getBus(), 350, 685 )
+      ->drawText( $costumer->getPostcode() . ' ' . $costumer->getGemeente() . ' ' . $costumer->getLandId(), 350, 674 )
+      ->drawText( 'IBAN ' . $costumer->getRekeningnummerIban() , 350, 663);
+
+    // Factuur gegevens
+    $pdf->pages[0]
+      ->setFont( $font , 12 )
+      ->setFillColor($cLightText)
+      ->drawText( 'Factuur ' . $invoice->getNumber(), $this->mmToPoints(16), 641 ); //Factuurnummer
+    $pdf->pages[0]
+      ->setFont( $font, 10)
+      ->drawText( $invoice->getDate('d-m-Y'), 480, 588 ) // factuurdatum
+      ->drawText( $invoice->getNumber(), 390, 588 ); // factuurnummer (Doc. Nr.)
+      //ontbreekt nog klantcode?
 
     ///////////
     ////////// Items:
     /////////
-    //Items Header
-    $pdf->pages[0]
-      ->setLineColor( $cDraw )
-      ->setFillColor( $cDraw )
-      ->drawRectangle( 0, 540, 510, 570 );
-    $pdf->pages[0]
-      ->setFont( $font, 10 )
-      ->setFillColor( $cWhite )
-      ->setLineColor( $cLightText )
-      ->setLineWidth( 0.5 )
-      ->drawText( 'Omschrijving ' , $this->mmToPoints(13) , 550 )
-      ->drawLine( 200, 540, 200, 570 )
-      ->drawText( '#', 210, 550 )
-      ->drawLine( 230, 540, 230, 570 )
-      ->drawText( 'Prijs/stuk' , 240, 550 )
-      ->drawLine( 290 , 540, 290, 570 )
-      ->drawText( 'Prijs (Excl)' , 300, 550 )
-      ->drawLine( 360, 540, 360, 570 )
-      ->drawText( 'BTW (%)', 370, 550 )
-      ->drawLine( 420, 540, 420, 570 )
-      ->drawText( 'Prijs (Incl)', 450, 550 );
-
     //Items
     $i = 0;
     $items = $invoice->getItems();
     foreach($items as $item)
     {
       $pdf->pages[0]
-        ->drawLine( 200, 510 - (30 * $i), 200, 540 - (30 * $i) )
-        ->drawLine( 230, 510 - (30 * $i), 230, 540 - (30 * $i) )
-        ->drawLine( 290, 510 - (30 * $i), 290, 540 - (30 * $i) )
-        ->drawLine( 360, 510 - (30 * $i), 360, 540 - (30 * $i) )
-        ->drawLine( 420, 510 - (30 * $i), 420, 540 - (30 * $i) )
-        ->drawLine( 510, 510 - (30 * $i), 510, 540 - (30 * $i) )
-        ->drawLine( 0, 510 - (30 * $i), 510, 510 - (30 * $i) );
-
-      $pdf->pages[0]
+        ->setFont( $font, 10 )
         ->setFillColor( $cText )
-        ->drawText( $item->getDescription(), $this->mmToPoints(13), 520 - (30 * $i) )
-        ->drawText( $item->getQuantity(), 210, 520 - (30 * $i) )
-        ->drawText( format_currency($item->getUnitPrice(), $currency), 240, 520 - (30 * $i), 'UTF-8' )
-        ->drawText( format_currency($item->getPriceExVat(), $currency), 300, 520 - (30 * $i), 'UTF-8' )
-        ->drawText( $item->getVat()->getPercentage() . '%', 370, 520 - (30 * $i) )
-        ->drawText( format_currency($item->getPriceInclVat(), $currency), 450, 520 - (30 * $i), 'UTF-8' );
+        ->drawText( $item->getDescription(), 50, 540 - (11 * $i) )
+        ->drawText( $item->getQuantity(), 250, 540 - (11 * $i) )
+        ->drawText( format_currency($item->getUnitPrice(), $currency), 340, 540 - (11 * $i), 'UTF-8' )
+        ->drawText( format_currency($item->getPriceExVat(), $currency), 410, 540 - (11 * $i), 'UTF-8' )
+        ->drawText( $item->getVat()->getPercentage() . '%', 520, 540 - (11 * $i) );
       $i++;
     }
 
     //Total
     $eindtotaal = $invoice->getTotal() + $invoice->getVat();
     $pdf->pages[0]
-      ->drawLine( 340, 510 - (30 * $i), 510, 510 - (30 * $i) )
-      ->drawText( 'Totaal (Excl)', 340, 520 - (30 * $i) )
-      ->drawText( format_currency($invoice->getTotal(), $currency), 440, 520 - (30 * $i), 'UTF-8' ) // totaal zonder btw
-      ->drawLine( 340, 480 - (30 * $i), 510, 480 - (30 * $i) )
-      ->drawText( 'BTW', 340, 490 - (30 * $i) )
-      ->drawText( format_currency($invoice->getVat(), $currency), 440, 490 - (30 * $i), 'UTF-8' ) //totaal BTW
-      ->drawLine( 340, 430 - (30 * $i), 510, 430 - (30 * $i) )
-      ->setFont( $font, 12 )
-      ->drawText( 'Totaal (Incl)', 340, 440 - (30 * $i) )
-      ->drawText( format_currency($eindtotaal, $currency), 440, 440 - (30 * $i), 'UTF-8' ); //Eindtotaal
+      ->drawText( format_currency($invoice->getTotal(), $currency), 490, 381, 'UTF-8' ) // totaal zonder btw
+      ->drawText( format_currency($invoice->getVat(), $currency), 490, 365, 'UTF-8' ) //totaal BTW
+      ->drawText( format_currency($eindtotaal, $currency), 490, 346, 'UTF-8' ) //Eindtotaal
+      ->drawText( $invoice->getDateDue('d-m-Y'), 120, 346 ) // vervaldag
+      ->drawText( $invoice->getStructuredCommunication(), 120 ,310 ); // gestructureerde mededeling
+      // ontbreekt nog totalen voor elke BTW apart
 
     ///////////
-    ////////// Footer:
+    ////////// Footer (overschrijvingsformulier):
     /////////
     $pdf->pages[0]
       ->setFont( $font, 10 )
-      ->drawText( 'Opmerkingen', $this->mmToPoints(13), 180 )
-      ->drawLine( 0, 170, 510, 170);
-
-    $para = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare tristique sem nec porta. In rutrum nibh nec vehicula ornare. Duis quis mollis urna. Nunc a porta ipsum. Sed nec mi nec justo sollicitudin malesuada. Donec diam libero, mollis a mauris vel, posuere porttitor elit. Curabitur aliquet tincidunt accumsan. Donec volutpat a erat et ultricies. Donec a imperdiet metus. Nam euismod convallis tincidunt. Nulla sem turpis, pretium et aliquet ac, hendrerit in diam. ';
-
-    $this->drawTextArea($pdf, $para, $this->mmToPoints(13), 150, 15, 110);
+      ->drawText( format_currency($eindtotaal), 500, 215, 'UTF-8' ) //totaalbedrag
+      ->drawText( $costumer->getNaam(), 130, 170 ) //ontvanger
+      ->drawText( $costumer->getStraat() . ' ' . $costumer->getNummer() . ' ' . $costumer->getBus(), 130, 159 )
+      ->drawText( $costumer->getPostcode() . ' ' . $costumer->getGemeente() . ' ' . $costumer->getLandId(), 130, 148 )
+      ->setFont( $overschrijvingsFont, 12 )
+      ->drawText( 'B E 4 8 7 3 1 0 0 7 7 9 4 3 2 7' , 104, 121 ) // IBAN Provincie
+      ->drawText( 'K R E D B E B B' , 104, 98 ) // BIC Provincie
+      ->setFont( $font , 10 )
+      ->drawText( 'APB SPORT', 130, 70 ) // gegevens Provincie
+      ->drawText( 'Boomgaardstraat 22', 130, 59 )
+      ->drawText( '2600 Berchem (Antwerpen) BE', 130, 48 )
+      ->drawText( $invoice->getStructuredCommunication(), 104 ,25 ); // gestructureerde mededeling
 
     return $pdf;
   }
@@ -160,7 +127,7 @@ class PdfCreator
 ///////////
 ////////// Tools:
 /////////
-  // Create wrapped text.
+  // Create wrapped text. (multiline)
   private function drawTextArea($pdf, $text, $pos_x, $pos_y, $height, $length = 0, $offset_x = 0, $offset_y = 0)
   {
     $x = $pos_x + $offset_x;
