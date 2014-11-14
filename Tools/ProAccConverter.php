2ab3a47040ba2070fdfe7ff03abdefd63e4141da
Joris Hontelé <joris@tactics.be>
2014-11-14 13:21:00 +0100

    [FEATURE] finished proAcc integratie

--- Tools/ProAccConverter.php
+++ Tools/ProAccConverter.php
@@ -3,6 +3,7 @@
 namespace Tactics\InvoiceBundle\Tools;
 
 use Tactics\InvoiceBundle\Model\Invoice;
+use Tactics\InvoiceBundle\Model\InvoiceItem;
 
 class ProAccConverter
 {
@@ -21,7 +22,7 @@ class ProAccConverter
     {
         $rangeAAToAM = array_map(create_function('$object', 'return "A{$object}";'), range('A', 'M'));
         $rangeAtoAM = array_merge(range('A', 'Z'), $rangeAAToAM);
-        $blancos = array_combine($rangeAtoAM, array_fill(0, count($rangeAtoAM), ''));
+        $blancos = array_combine($rangeAtoAM, array_fill(0, count($rangeAtoAM), '0'));
         
         $lines = array();
         foreach ($this->invoice->getItems() as $cnt => $item)
@@ -29,24 +30,30 @@ class ProAccConverter
             $lines[] = array_merge($blancos, array(
               'A' => $cnt === 0 ? '1' : '3',
               'B' => $this->getKlantcode(),
-              'C' => $this->invoice->getJournalCode(),
+              'C' => $this->getJournalNumber($item),
               'D' => $this->invoice->getNumber(),
               'E' => $this->invoice->getDate('d/m/Y'),
-              'F' => '1403', // 2014 kwartaal 3???
+              'F' => $this->invoice->getDate('ym'),
+              'G' => '',
               'H' => $this->invoice->getDateDue('d/m/Y'),
               'I' => 'EUR',
+              'J' => 1,
+              'K' => number_format($this->invoice->getTotal() + $this->invoice->getVat(), 2, ',', ''),
               'L' => number_format($this->invoice->getTotal() + $this->invoice->getVat(), 2, ',', ''),
               'M' => number_format($this->invoice->getTotal(), 2, ',', ''),
               'N' => number_format($this->invoice->getVat(), 2, ',', ''),
+              'X' => $this->invoice->getVat() ? number_format($this->invoice->getTotal(), 2, ',', '') : 0,00,
+              'Z' => $item->getAnalytical1Account() ? $item->getAnalytical1Account()->getName() : '',
               'AA' => $item->getGlAccount()->getCode(),
               'AB' => $item->getAnalytical1Account() ? $item->getAnalytical1Account()->getCode() : '',
-              'AC' => number_format($item->getUnitPrice(), 2, ',', ''),
-              'AD' => number_format($item->getUnitPrice(), 2, ',', ''),
+              'AC' => number_format($item->getPriceExVat(), 2, ',', ''),
+              'AD' => number_format($item->getPriceExVat(), 2, ',', ''), // idem als AC - fin.korting, maar fin.korting wordt niet gebruikt
               'AE' => number_format($item->getVat()->getPercentage(), 2, ',', ''),
-              'AF' => $item->getQuantity(),
               'AG' => substr($item->getGlAccount()->getName(), 0, 25),
               'AI' => $item->getAnalytical2Account() ? $item->getAnalytical2Account()->getCode() : '',
-              'AL' => $this->invoice->getDatePaid() ? '1' : '0'
+              'AK' => '',
+              'AL' => $this->invoice->getDatePaid() ? '1' : '0',
+              'AM' => ''
             ));
         }
         
@@ -62,11 +69,51 @@ class ProAccConverter
         ));
     }
     
+    /**
+     * Geeft de klantcode terug
+     * 
+     * @return string
+     */
     private function getKlantcode()
     {
         $prefix = get_class($this->invoice->getCustomer()) === 'Organisatie' ? 'O' : 'P';
         
         return sprintf("$prefix%06u", $this->invoice->getCustomer()->getId());
+    }
+    
+    /**
+     * Geeft het dagboeknummer terug
+     * 1. facturen verkoopdagboek
+     * 2. creditnota's verkoopdagboek
+     * 3. Facturen vrij van BTW
+     * 4. CN vrij van BTW
+     * 
+     * @return int
+     */
+    private function getJournalNumber(InvoiceItem $item)
+    {
+      $isCN = bccomp($item->getPriceExVat(), 0, 2) === -1;
+      $vrijVanBtw = $item->getPriceExVat() && !$item->getVat();
+      
+      if ($isCN && $vrijVanBtw)
+      {
+        return 4;
+      }
+      
+      if ($isCN && !$vrijVanBtw)
+      {
+        return 2;
+      }
+      
+      if (!$isCN && $vrijVanBtw)
+      {
+        return 3;
+      }
+      
+      if (!$isCN && !$vrijVanBtw)
+      {
+        return 1;
+      }
     }
 }
 
