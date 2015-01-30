abef203d3e625a073da4e7194ecdde909126f9fc
Joris Hontelé <joris@tactics.be>
2015-01-30 13:48:08 +0100

    [ENHANCE] Journal: added with_vat and credit_notes

--- Model/Journal.php
+++ Model/Journal.php
@@ -6,6 +6,8 @@ class Journal
 {
     protected $code;    
     protected $name;
+    protected $with_vat = false;
+    protected $credit_notes = false;
     protected $scheme_id;
 
     // getters
@@ -22,6 +24,16 @@ class Journal
     public function getSchemeId()
     {
         return $this->scheme_id;
+    }
+    
+    public function getWithVat()
+    {
+      return $this->with_vat;
+    }
+    
+    public function getCreditNotes()
+    {
+      return $this->credit_notes;
     }
   
     // setters
@@ -47,6 +59,16 @@ class Journal
 	{
         $this->scheme_id = $v;
 	}
+  
+    public function setWithVat($withVat)
+    {
+        $this->with_vat = $withVat;
+    }
+    
+    public function setCreditNotes($creditNotes)
+    {
+        $this->credit_notes = $creditNotes;
+    }
     
     public function __toString()
     {
