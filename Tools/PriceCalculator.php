f14a44fe7e7fb27773945f1ea12006796fcd64d2
Joris Hontelé <joris@tactics.be>
2016-05-30 13:44:38 +0200

    fix prijsafronding

--- Tools/PriceCalculator.php
+++ Tools/PriceCalculator.php
@@ -9,8 +9,8 @@ class PriceCalculator
     public function calculate(InvoiceItem $item)
     {
         $item->setPriceExVat(bcmul($item->getQuantity(), $item->getUnitPrice(), 2));
-        $vatMultiplier = bcdiv(bcadd(100, $item->getVatPercentage(), 2), 100, 2);
-        $item->setPriceInclVat(bcmul($item->getPriceExVat(), $vatMultiplier, 2));
-        $item->setVat(bcsub($item->getPriceInclVat(), $item->getPriceExVat(), 2));
+        $vatMultiplier = bcdiv($item->getVatPercentage(), 100, 2);
+        $item->setVat(round(bcmul($item->getPriceExVat(), $vatMultiplier, 3), 2));
+        $item->setPriceInclVat(bcadd($item->getPriceExVat(), $item->getVat(), 2));
     }
 }
