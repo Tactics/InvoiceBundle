3f3ef900f934c99c9ffba812d123d9a9d9123b66
Joris Hontelé <joris@tactics.be>
2016-10-21 13:57:31 +0200

    added function getCustomerInfoValue()

--- Tools/Customer.php
+++ Tools/Customer.php
@@ -2,6 +2,7 @@
 
 namespace Tactics\InvoiceBundle\Tools;
 
+use Tactics\InvoiceBundle\Model\SchemeCustomerInfo;
 use Tactics\InvoiceBundle\Propel\ObjectManager;
 
 abstract class Customer
@@ -13,12 +14,12 @@ abstract class Customer
     protected $customerInfo;
     
     /**
-     * 
+     *
      * @param mixed $customer the application customer
      * @param ObjectManager $customerInfoManager
      */
     public function __construct($customer, ObjectManager $customerInfoManager)
-    {        
+    {
         $this->customer = $customer;
         $this->customerClass = get_class($customer);
         $this->customerInfoManager = $customerInfoManager;
@@ -30,14 +31,13 @@ abstract class Customer
     }
     
     /**
-     * 
+     *
      * @param int $schemeId The accounting scheme id
-     * @return array[name] = value
+     * @return SchemeCustomerInfo[]
      */
     public function getCustomerInfo($schemeId)
     {
-        if (!$this->customerInfo)
-        {
+        if (!$this->customerInfo) {
             $storedInfo = $this->findStoredCustomerInfo($schemeId);
             $defaults = array_combine($this->getFields(), array_fill(0, count($this->getFields()), null));
             $this->customerInfo = array_intersect_key(array_merge($defaults, $storedInfo), $defaults);
@@ -47,19 +47,39 @@ abstract class Customer
     }
     
     /**
-     * 
+     * @param int $schemeId
+     * @param string $field
+     */
+    public function getCustomerInfoValue($schemeId, $field)
+    {
+        if (!in_array($field, $this->getFields())){
+            throw new \Exception("$field is geen geldig veld.");
+        }
+        $customerInfo = $this->getCustomerInfo($schemeId);
+        return isset($customerInfo[$field]) ? $customerInfo[$field]->get() : '';
+    }
+    
+    /**
+     *
      * @param int $schemeId The accounting scheme id
      * @return array[name] = value
      */
     private function findStoredCustomerInfo($schemeId)
     {
         $infos = $this->customerInfoManager->search(array(
-          'scheme_id' => $schemeId,
-          'customer_id' => $this->customer->getId(), 
-          'customer_class' => $this->customerClass
+            'scheme_id' => $schemeId,
+            'customer_id' => $this->customer->getId(),
+            'customer_class' => $this->customerClass
         ));
         
-        return array_combine(array_map(function($info){return $info->getName();}, $infos), $infos);
+        return array_combine(array_map(function ($info) {
+            return $info->getName();
+        }, $infos), $infos);
     }
+    
+    /**
+     * @return string[]
+     */
+    abstract function getFields();
 }
 
